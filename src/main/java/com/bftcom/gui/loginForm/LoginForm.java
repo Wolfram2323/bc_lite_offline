769b20909253ea50f9ef3785671d165a47f9899f
k.nikitin <k.nikitin@bftcom.com>
2016-11-14 15:21:48 +0300

    FindDerby

--- src/main/java/com/bftcom/gui/loginForm/LoginForm.java
+++ src/main/java/com/bftcom/gui/loginForm/LoginForm.java
@@ -5,11 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFileFilter;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 /**
  * Created by k.nikitin on 03.11.2016.
@@ -25,6 +24,18 @@ public class LoginForm extends Application {
         primaryStage.setResizable(false);
         primaryStage.setTitle("Авторизация");
         primaryStage.show();
+    }
+
+    public void findDerby(){
+        File home = new File("./");
+        File[] resultSearch = home.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File pathname,String fileName) {
+                return fileName.startsWith("derby_");
+            }
+        });
+        File derbyDir = resultSearch[0];
+        System.setProperty("derby.system.home", derbyDir.getAbsolutePath());
     }
 
 }
